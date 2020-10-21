0ac911355ff909633a694140c7e0202fefc74f2a
LYT <e0003133@u.nus.edu>
2020-10-21 12:30:40 +0800

    upload initial files

---
+++ buildtagger.py
@@ -0,0 +1,21 @@
+# python3.5 buildtagger.py <train_file_absolute_path> <model_file_absolute_path>
+
+import os
+import math
+import sys
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+def train_model(train_file, model_file):
+    # write your code here. You can add functions as well.
+		# use torch library to save model parameters, hyperparameters, etc. to model_file
+    print('Finished...')
+		
+if __name__ == "__main__":
+    # make no changes here
+    train_file = sys.argv[1]
+    model_file = sys.argv[2]
+    train_model(train_file, model_file)
